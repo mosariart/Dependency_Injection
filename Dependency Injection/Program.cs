6e26cac3f764905a1f44bd39a28a416c233fd2fc
mohsen.ansari <mohsen.ansari.bim@gmail.com>
2022-08-30 20:03:32 +0430

    Add project files.

---
+++ Dependency Injection/Program.cs
@@ -0,0 +1,76 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+
+namespace AutofacSamples
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private Engine engine;
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.engine = engine;
+            this.log = log;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(100);
+            log.Write("Car going forward...");
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleLog>().As<ILog>().AsSelf();
+            //builder.RegisterType<Engine>();
+            builder.RegisterType<Car>();
+
+            IContainer a = builder.Build();
+
+            var b = a.Resolve<Car>();
+            b.Go();
+            Console.ReadKey();
+
+            //var log = new ConsoleLog();
+            //var engine = new Engine(log);
+            //var car = new Car(engine, log);
+            //car.Go();
+        }
+    }
+}
