a4a3dabab93d87414963ca15851436c1f30759e6
mohsen.ansari <mohsen.ansari.bim@gmail.com>
2022-09-01 16:30:52 +0430

    Property & Method injection (using Parent and Child Classes)

---
+++ Dependency Injection/Models/Child.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofacSamples
+{
+    public class Child
+    {
+        public string Name { get; set; }
+        public Parent Parent { get; set; }
+
+        public void SetParent(Parent parent)
+        {
+            Parent = parent;
+        }
+    }
+}
--- Dependency Injection/Program.cs
+++ Dependency Injection/Program.cs
@@ -6,18 +6,41 @@ using System.Collections.Generic;
 namespace AutofacSamples
 {
     internal class Program
-    {
+    { 
         public static void Main(string[] args)
         {
-            //delegate factories allow you to create objects on demand
-            var cb = new ContainerBuilder();
-            cb.RegisterType<Entity>().InstancePerDependency();
-            cb.RegisterType<ViewModel>();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Parent>();
 
-            var container = cb.Build();
-            var vm = container.Resolve<ViewModel>();
-            vm.Method();
-            vm.Method();
+            //Below we have to ways of property injection
+
+            //the first way
+            //here the system will go through all the properties and try to resolve it
+            //builder.RegisterType<Child>().PropertiesAutowired();
+
+            //the second way 
+            //builder.RegisterType<Child>()
+            //    .WithProperty("Parent", new Parent());
+
+            //var container = builder.Build();
+            //var parent = container.Resolve<Child>().Parent;
+            //Console.WriteLine(parent);
+            //Console.ReadKey();
+
+            //============================================================//
+            //Below we talk about method dependency.
+            //which means a method which is defined to inject the dependency.
+            //in this way we are not only registering a class but also registering
+            //it in a way that we have assigned a property by calling a particular method.
+            builder.Register(c =>
+            {
+                var child = new Child();
+                child.SetParent(c.Resolve<Parent>());
+                return child;
+            });
+            var container = builder.Build();
+            var parent = container.Resolve<Child>().Parent;
+            Console.WriteLine(parent);
             Console.ReadKey();
         }
     }
